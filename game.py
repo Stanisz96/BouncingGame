439dbcdd7cb0fc9a08c6a2d24d9eb1aa109e2897
Michal Staniszewski <stanisz96@gmail.com>
2019-10-18 20:29:31 +0200

    Create ball and paddle
    Add two classes: ball and paddle. Create object in while loop.
    
    Class Ball contains:
    - def __init__ -> declare variable
    - def hit_paddle -> check hit between ball and paddle
    - def draw -> change movement direction
    
    Class Paddle contains:
    - def __init__ -> declare variable and  bind a callback
    function to an event
    - def draw -> change movement direction and prevent exit from the canvas
    - def "X-Key" -> change movement depending on the event
    
    Expand while loop to:
    - check hit between ball and paddle
    - ball draw
    - paddle draw

--- game.py
+++ game.py
@@ -41,8 +41,107 @@ canvas.pack()
 root.update()
 
 
+class Ball:
+    def __init__(self,canvas,color,paddle):
+        self.canvas = canvas
+        self.paddle = paddle
+        self.id = canvas.create_oval(0,0,14,14, fill=color)
+        self.canvas.move(self.id,243,93)
+        self.x = 4
+        self.y = -4
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+    def hit_paddle(self):
+        ball_pos = self.canvas.coords(self.id)
+        paddle_pos = self.paddle.canvas.coords(self.paddle.id)
+        if ball_pos[3] >= paddle_pos[1] and ball_pos[3] <= paddle_pos[3]:
+            if ball_pos[2] >= paddle_pos[0] and ball_pos[0] <= paddle_pos[2]:
+                self.y *= -3
+            else: pass
+        else: pass
+
+    def draw(self):
+        self.canvas.move(self.id,self.x,self.y)
+        ball_pos = self.canvas.coords(self.id)
+        if ball_pos[1] <= 0:
+            self.y = 4
+        if ball_pos[3] >= self.canvas_height:
+            self.y = -4
+        if ball_pos[0] <= 0:
+            self.x = 4
+        if ball_pos[2] >= self.canvas_width:
+            self.x = -4
+
+
+class Paddle:
+    def __init__(self,canvas,color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0,0,120,15, fill=color)
+        self.canvas.move(self.id,190,340)
+        self.x = 0
+        self.y = 0
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all("<KeyPress-Left>",self.leftKey)
+        self.canvas.bind_all("<KeyPress-Right>", self.rightKey)
+        self.canvas.bind_all("<KeyPress-Up>", self.upKey)
+        self.canvas.bind_all("<KeyPress-Down>", self.downKey)
+        self.canvas.bind_all("<KeyRelease-Left>",self.let_right_Release)
+        self.canvas.bind_all("<KeyRelease-Right>", self.let_right_Release)
+        self.canvas.bind_all("<KeyRelease-Up>", self.up_down_Release)
+        self.canvas.bind_all("<KeyRelease-Down>", self.up_down_Release)
+
+    def draw(self):
+        self.canvas.move(self.id,self.x,self.y)
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[1] <= 0:
+            self.y = 0
+        if paddle_pos[3] >= self.canvas_height:
+            self.y = 0
+        if paddle_pos[0] <= 0:
+            self.x = 0
+        if paddle_pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def leftKey(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[0] >= 0:
+            self.x = -3
+        else:
+            self.x = 0
+    def rightKey(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[2] <= self.canvas_width:
+            self.x = 3
+        else:
+            self.x = 0
+    def upKey(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[1] >= 0:
+            self.y = -3
+        else:
+            self.y = 0
+    def downKey(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[3] <= self.canvas_height:
+            self.y = 3
+        else:
+            self.y = 0
+    def let_right_Release(self, event):
+        self.x *= 0.2
+    def up_down_Release(self, event):
+        self.y *= 0.2
+
+
+paddle = Paddle(canvas,"green")
+ball = Ball(canvas,"red",paddle)
+
 
 while x == True:
+    ball.hit_paddle()
+    ball.draw()
+    paddle.draw()
     root.update_idletasks()
     root.update()
     time.sleep(0.02)
