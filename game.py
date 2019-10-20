abb4b270b591b749397fecb5fff393f4a5ad35c9
Michal Staniszewski <stanisz96@gmail.com>
2019-10-20 12:07:16 +0200

    Interface and game adjustment v2
    Game:
    - Create new function which handle "New Game" event
    - Add conditions for key release and edit functions key press.
      Paddle is not hidding in canvas border
    
    Interface:
    - Create class GUI for interface
    - "New Game" button now start new game

--- game.py
+++ game.py
@@ -3,47 +3,45 @@ import tkinter.messagebox
 import time
 import random
 
-# Main window of an application
-root = Tk()
-root.title("Bounce!")
-root.resizable(0,0)
-root.wm_attributes('-topmost',1)
-
-# Properly closed window
-x = True
-
-def update_x():
-    global x
-    x = False
-
-root.protocol("WM_DELETE_WINDOW", update_x)
-
-# Top frame with informations
-topFrame = Frame(root,bd=3, relief=SUNKEN)
-topFrame.pack(pady=2)
-
-#Top frame contains
-##Score information
-label1 = Label(topFrame, text="SCORE:",height=2,width=9,relief=SUNKEN).grid(row=0,column=0)
-label2 = Label(topFrame, text="H.SCORE:",height=2,width=9,relief=SUNKEN).grid(row=1,column=0)
-label3 = Label(topFrame, text="XX",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=0,column=1)
-label4 = Label(topFrame, text="YY",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=1,column=1)
-##New Game
-button1 = Button(topFrame,width=24,bg="#F7D952",pady=8,text="NEW GAME",relief=GROOVE).grid(row=0,column=2,columnspan=2)
-##Speed and points information
-label5 = Label(topFrame, text=" SPEED:",height=2,width=12,relief=GROOVE,anchor=W).grid(row=1,column=2)
-label6 = Label(topFrame, text=" POINTS:",height=2,width=12,relief=GROOVE,anchor=W).grid(row=1,column=3)
-##Level information
-label7 = Label(topFrame, text="LEVEL:",height=2,width=9,relief=SUNKEN).grid(row=0,column=4)
-label8 = Label(topFrame, text="H.LEVEL:",height=2,width=9,relief=SUNKEN).grid(row=1,column=4)
-label9 = Label(topFrame, text="XX",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=0,column=5)
-label10 = Label(topFrame, text="YY",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=1,column=5)
 
 
-# Canvas for game
-canvas = Canvas(root, width=500,height=500,bd=2,highlightthickness=1,bg="#E7DEFB",highlightbackground="#8BA16E",relief=SUNKEN)
-canvas.pack()
-root.update()
+class GUI:
+    def __init__(self,master):
+        self.master = master
+        # Properly closed window
+        self.x = True
+        self.master.protocol("WM_DELETE_WINDOW", self.update_x)
+        # Top frame with informations
+        self.topFrame = Frame(self.master,bd=3, relief=GROOVE)
+        self.topFrame.pack(pady=2)
+
+        #Top frame contains
+        ##Score information
+        self.label1 = Label(self.topFrame, text="SCORE:",height=2,width=8,relief=SUNKEN).grid(row=0,column=0)
+        self.label2 = Label(self.topFrame, text="H.SCORE:",height=2,width=8,relief=SUNKEN).grid(row=1,column=0)
+        self.label3 = Label(self.topFrame, text="XX",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=0,column=1)
+        self.label4 = Label(self.topFrame, text="YY",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=1,column=1)
+        ##New Game
+        self.new_Game = False
+        self.button1 = Button(self.topFrame,width=24,bg="#F7D952",pady=8,text="NEW GAME",relief=GROOVE,command=self.newGame).grid(row=0,column=2,columnspan=2)
+        ##Speed and points information
+        self.label5 = Label(self.topFrame, text=" SPEED:",height=2,width=12,relief=GROOVE,anchor=W).grid(row=1,column=2)
+        self.label6 = Label(self.topFrame, text=" POINTS:",height=2,width=12,relief=GROOVE,anchor=W).grid(row=1,column=3)
+        ##Level information
+        self.label7 = Label(self.topFrame, text="LEVEL:",height=2,width=9,relief=SUNKEN).grid(row=0,column=4)
+        self.label8 = Label(self.topFrame, text="H.LEVEL:",height=2,width=9,relief=SUNKEN).grid(row=1,column=4)
+        self.label9 = Label(self.topFrame, text="XX",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=0,column=5)
+        self.label10 = Label(self.topFrame, text="YY",height=2,width=12,anchor=W,relief=SUNKEN,padx=4).grid(row=1,column=5)
+
+        # Canvas for game
+        self.canvas = Canvas(self.master, width=500,height=500,bd=2,highlightthickness=1,bg="#E7DEFB",highlightbackground="#8BA16E",relief=SUNKEN)
+        self.canvas.pack()
+
+    def update_x(self):
+        self.x = False
+
+    def newGame(self):
+        self.new_Game = True
 
 
 class Ball:
@@ -77,7 +75,7 @@ class Ball:
             self.canvas.coords(self.id,[244,44,256, 56])
             self.x = 0
             self.y = 0
-            tkinter.messagebox.showinfo(message="GAME OVER!")
+            #tkinter.messagebox.showinfo(message="GAME OVER!")
         if ball_pos[0] <= 0:
             self.x = 4
         if ball_pos[2] >= self.canvas_width:
@@ -97,61 +95,98 @@ class Paddle:
         self.canvas.bind_all("<KeyPress-Right>", self.rightKey)
         self.canvas.bind_all("<KeyPress-Up>", self.upKey)
         self.canvas.bind_all("<KeyPress-Down>", self.downKey)
-        self.canvas.bind_all("<KeyRelease-Left>",self.let_right_Release)
-        self.canvas.bind_all("<KeyRelease-Right>", self.let_right_Release)
-        self.canvas.bind_all("<KeyRelease-Up>", self.up_down_Release)
-        self.canvas.bind_all("<KeyRelease-Down>", self.up_down_Release)
+        self.canvas.bind_all("<KeyRelease-Left>",self.leftRelease)
+        self.canvas.bind_all("<KeyRelease-Right>", self.rightRelease)
+        self.canvas.bind_all("<KeyRelease-Up>", self.upRelease)
+        self.canvas.bind_all("<KeyRelease-Down>", self.downRelease)
 
     def draw(self):
         self.canvas.move(self.id,self.x,self.y)
         paddle_pos = self.canvas.coords(self.id)
-        if paddle_pos[1] <= 0:
+        if paddle_pos[1] <= 6:
             self.y = 0
-        if paddle_pos[3] >= self.canvas_height:
+        if paddle_pos[3] >= self.canvas_height-6:
             self.y = 0
-        if paddle_pos[0] <= 0:
+        if paddle_pos[0] <= 6:
             self.x = 0
-        if paddle_pos[2] >= self.canvas_width:
+        if paddle_pos[2] >= self.canvas_width-6:
             self.x = 0
 
     def leftKey(self, event):
         paddle_pos = self.canvas.coords(self.id)
-        if paddle_pos[0] >= 0:
+        if paddle_pos[0] >= 6:
             self.x = -3
         else:
             self.x = 0
     def rightKey(self, event):
         paddle_pos = self.canvas.coords(self.id)
-        if paddle_pos[2] <= self.canvas_width:
+        if paddle_pos[2] <= (self.canvas_width - 6):
             self.x = 3
         else:
             self.x = 0
     def upKey(self, event):
         paddle_pos = self.canvas.coords(self.id)
-        if paddle_pos[1] >= 0:
+        if paddle_pos[1] >= 6:
             self.y = -3
         else:
             self.y = 0
     def downKey(self, event):
         paddle_pos = self.canvas.coords(self.id)
-        if paddle_pos[3] <= self.canvas_height:
+        if paddle_pos[3] <= (self.canvas_height - 6):
             self.y = 3
         else:
             self.y = 0
-    def let_right_Release(self, event):
-        self.x *= 0.3
-    def up_down_Release(self, event):
-        self.y *= 0.3
+    def leftRelease(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[0] >= 6:
+            self.x *= 0.3
+        else:
+            self.x = 0
+    def rightRelease(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[2] <= (self.canvas_width - 6):
+            self.x *= 0.3
+        else:
+            self.x = 0
+    def upRelease(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[1] >= 6:
+            self.y *= 0.3
+        else:
+            self.y = 0
+    def downRelease(self, event):
+        paddle_pos = self.canvas.coords(self.id)
+        if paddle_pos[3] <= (self.canvas_height - 6):
+            self.y *= 0.3
+        else:
+            self.y = 0
 
 
-paddle = Paddle(canvas,"green")
-ball = Ball(canvas,"red",paddle)
+# Main window for application
+root = Tk()
+root.title("Bounce!")
+root.resizable(0, 0)
+root.wm_attributes('-topmost', 1)
+
+#Create gui object
+gui = GUI(root)
+root.update()
+#Create paddle and ball
+paddle = Paddle(gui.canvas,"green")
+ball = Ball(gui.canvas,"red",paddle)
 
 
-while x == True:
+while gui.x == True:
+    if gui.new_Game == True:
+        del ball, paddle
+        gui.canvas.delete("all")
+        paddle = Paddle(gui.canvas, "green")
+        ball = Ball(gui.canvas, "red", paddle)
+        gui.new_Game = False
     ball.hit_paddle()
     paddle.draw()
     ball.draw()
-    root.update_idletasks()
-    root.update()
+    gui.master.update_idletasks()
+    gui.master.update()
     time.sleep(0.02)
+
